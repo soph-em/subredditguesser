b54e6ed90e64f9f4e20f1059bfec2b992c51b3eb
Riacule <sophie.earl@live.com>
2023-02-06 18:30:08 +0000

    description update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,9 +7,11 @@
 		<h1>How to play</h1>
 
 		<p>
-			Guess the subreddit the image is from. The list on the right gives options and will strike
-			through incorrect guesses. If you get stuck press the 'Hint' button to reveal the title of the
-			post.
+			Subreddit guesser for reddit. Not created by or associated with Reddit, Inc.
+			<br />
+			<br />
+			Guess the subreddit the image is from. The list on the right gives options and will strike through
+			incorrect guesses. If you get stuck press the 'Hint' button to reveal the title of the post.
 			<br />
 			<br />
 			Play on daily mode to get a daily challenge and share how you did with friends. Play unlimited
